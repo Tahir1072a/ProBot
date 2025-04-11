670ab98236caf9c039435a20b141e6ec4df947e2
Tahir <tahirifdn@gmail.com>
2025-04-11 20:50:29 +0300

    Imu orientation değeri düzeltildi. Ayrıca kalman filter node'u eklendi ve tek boyutlu tahmin yapıldı

--- bumperbot_localization/bumperbot_localization/kalman_filter.py
+++ bumperbot_localization/bumperbot_localization/kalman_filter.py
@@ -15,12 +15,24 @@ class KalmanFilter(Node):
         self.mean = 0.0
         self.variance = 1000.0
         
-        self.imu_angular_z = 0.0
+        self.imu_angular_z = 0.0 # Imu'dan gelen son angular_z mesajını depolar
         self.is_first_odom = True
-        self.last_angular_z = 0.0 #odometry noisy topic'inden gelen değeri depolamak için.
+        self.last_angular_z = 0.0 # odometry noisy topic'inden gelen değeri depolamak için.
 
-        self.motion = 0.0
+        self.motion = 0.0 # Ardışık zamanda gelen iki angular_velocity değerinin farkını depolayacaktır.
         self.kalman_odom = Odometry()
+
+        self.motion_variance = 4.0
+        self.measurement_variance = 0.5
+
+    def measurement_update(self):
+        self.mean = (self.measurement_variance * self.mean + self.variance * self.imu_angular_z) / (self.variance + self.measurement_variance)
+        self.variance = (self.variance * self.measurement_variance) / (self.variance + self.measurement_variance)
+    
+    # Bir sonraki adımı tahmin eder. Yani bir sonraki adımda oluşacak olan gauss dağılımını hesaplarız.
+    def state_prediction(self):
+        self.mean = self.mean + self.motion
+        self.variance = self.variance + self.motion_variance
 
     def imu_callback(self, imu):
         self.imu_angular_z = imu.angular_velocity.z
@@ -29,13 +41,29 @@ class KalmanFilter(Node):
         self.kalman_odom = odom
 
         if self.is_first_odom:
-            self.mean = odom.twist.twist.angular.z
             self.last_angular_z = odom.twist.twist.angular.z
+            self.mean = odom.twist.twist.angular.z
 
             self.is_first_odom = False
             return
 
-        self.statePrediction() # implemente edilecek
+        self.motion = odom.twist.twist.angular.z - self.last_angular_z
 
-        self.mesaurementUpdate() # implemente edilecek
+        self.state_prediction() 
+        self.measurement_update() 
+
+        self.last_angular_z = odom.twist.twist.angular.z
+
+        self.kalman_odom.twist.twist.angular.z = self.mean
+        self.odom_pub.publish(self.kalman_odom)
+
+def main():
+    rclpy.init()
+    node = KalmanFilter()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
     
